180f604ad8e5494b3e5957f3b87898d0c8270e5d
Ain Tohvri <ain@flashbit.net>
2020-12-14 23:56:43 +0100

    Add API constants for auth basis

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+
+const API_KEY:&str = "";
+const API_URL:&str = "";
+
 fn main() {
-    println!("Hello, world!");
+    if API_KEY.is_empty() || API_URL.is_empty() {
+        panic!("Invalid API credentials!");
+    }
 }
